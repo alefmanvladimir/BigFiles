b361dd2138792b3acb71e0d41d1d066af658b74f
Nikolay Dorofeev <dorich2000@gmail.com>
2023-10-14 15:01:54 +0400

    Beautify files sorting

--- ton-drive-frontend/src/features/file/ui/FilesSortControls.tsx
+++ ton-drive-frontend/src/features/file/ui/FilesSortControls.tsx
@@ -2,13 +2,14 @@ import { useState } from "react"
 import type { TonStorageFile } from "../../../entities/file/model/TonStorageFile"
 
 export interface FilesSortControlProps {
+  className?: string
   files: TonStorageFile[]
   onSort?: (sortedFiles: TonStorageFile[]) => void
 }
 
 type SortAttribute = 'name' | 'date' | 'size'
 
-export default function FilesSortControls({ files, onSort }: FilesSortControlProps){
+export default function FilesSortControls({ files, onSort, className = '' }: FilesSortControlProps){
   const [areDatesAscending, setAreDatesAscending] = useState(false)
   const [areNamesAscending, setAreNamesAscending] = useState(true)
   const [areSizesAscending, setAreSizesAscending] = useState(true)
@@ -16,14 +17,14 @@ export default function FilesSortControls({ files, onSort }: FilesSortControlPro
 
   return (
     <>
-      <div className="join">
-        <button className={`btn join-item ${sortAttribute === 'date' ? 'btn-active' : ''}`} onClick={() => onButtonClick('date')}>
+      <div className={`join ${className}`}>
+        <button className={`btn btn-sm join-item ${sortAttribute === 'date' ? 'btn-active btn-accent' : ''}`} onClick={() => onButtonClick('date')}>
           <SortOrderIcon asc={areDatesAscending} /> Date
         </button>
-        <button className={`btn join-item ${sortAttribute === 'name' ? 'btn-active' : ''}`} onClick={() => onButtonClick('name')}>
+        <button className={`btn btn-sm join-item ${sortAttribute === 'name' ? 'btn-active btn-accent' : ''}`} onClick={() => onButtonClick('name')}>
           <SortOrderIcon asc={areNamesAscending} /> Name
         </button>
-        <button className={`btn join-item ${sortAttribute === 'size' ? 'btn-active' : ''}`} onClick={() => onButtonClick('size')}>
+        <button className={`btn btn-sm join-item ${sortAttribute === 'size' ? 'btn-active btn-accent' : ''}`} onClick={() => onButtonClick('size')}>
           <SortOrderIcon asc={areSizesAscending} /> Size
         </button>
       </div>
@@ -106,10 +107,16 @@ function SortOrderIcon({ asc }: SortOrderIconProps) {
       {
         asc ?
           <>
-            ▲
+            {/* arrow up */}
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12" />
+            </svg>
           </>:
           <>
-            ▼
+            {/* arrow down */}
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25" />
+            </svg>
           </>
       }
     </>
