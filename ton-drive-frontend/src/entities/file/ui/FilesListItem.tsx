7eecb20b4e9c69896b2bbc5a735270fe810b7e83
Nikolay Dorofeev <dorich2000@gmail.com>
2023-10-15 11:30:46 +0400

    Different files extensions icons

--- ton-drive-frontend/src/entities/file/ui/FilesListItem.tsx
+++ ton-drive-frontend/src/entities/file/ui/FilesListItem.tsx
@@ -1,6 +1,7 @@
 import type { TonStorageFile } from "../model/TonStorageFile"
 import FileSize from "./FileSize"
 import FileDate from "./FileDate"
+import FileTypeIcon from "./FileTypeIcon"
 
 export interface FilesListItemProps {
   file: TonStorageFile
@@ -12,10 +13,7 @@ export default function FilesListItem({ file, className, actions }: FilesListIte
   return (
     <li className={`card card-compact flex-row items-center gap-2 bg-base-300 p-3 ${className ?? ''}`}>
       <div>
-        {/* Document icon */}
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-        </svg>
+        <FileTypeIcon extension={file.extension} />
       </div>
       <div className="text-ellipsis overflow-hidden">
         <div className="card-title">
--- ton-drive-frontend/src/widgets/hooks/useUserFile.ts
+++ ton-drive-frontend/src/widgets/hooks/useUserFile.ts
@@ -11,8 +11,16 @@ export function useUserFiles(): TonStorageFile[] {
 
 async function fetchFiles(): Promise<TonStorageFile[]> {
   return [
-    { bagId: "8A3BFBAC38A688322EC507C06F90D5FA74DEECEEEB2E40666D25DA89291F4930", name: "file1", extension: "txt", size: 100, date: new Date('2021-08-03') },
-    { bagId: "8A3BFBAC38A688322EC507C06F90D5FA74DEECEEEB2E40666D25DA89291F4931", name: "file2", extension: "txt", size: 200, date: new Date('2022-08-03') },
-    { bagId: "8A3BFBAC38A688322EC507C06F90D5FA74DEECEEEB2E40666D25DA89291F4932", name: "file3", extension: "txt", size: 300, date: new Date('2023-08-03') },
+    { bagId: "8A3BFBAC38A688322EC507C06F90D5FA74DEECEEEB2E40666D25DA89291F4930", name: "file1", extension: "gif", size: 100, date: new Date('2021-08-03') },
+    { bagId: "8A3BFBAC38A688322EC507C06F90D5FA74DEECEEEB2E40666D25DA89291F4931", name: "file2", extension: "jpeg", size: 200, date: new Date('2022-08-03') },
+    { bagId: "8A3BFBAC38A688322EC507C06F90D5FA74DEECEEEEB2E40666D25DA89291F4932", name: "file3", extension: "pdf", size: 300, date: new Date('2023-08-03') },
+    { bagId: "8A3BFBAC38A688322EC507C06F90D5FA74DEECEEEEB2E40666D25DA89291F4942", name: "data", extension: "json", size: 300, date: new Date('2023-08-02') },
+    { bagId: "8A3BFBAC38A688322EC507C06F90D5FA74DEECEEEEB2E40666D25DA89291F4933", name: "file4", extension: "mp3", size: 400_000, date: new Date('2023-07-03') },
+    { bagId: "8A3BFBAC38A688322EC507C06F90D5FA74DEECEEEEB2E40666D25DA89291F4934", name: "file5", extension: "xls", size: 500_000, date: new Date('2023-06-03') },
+    { bagId: "8A3BFBAC38A688322EC507C06F90D5FA74DEECEEEEB2E40666D25DA89291F4935", name: "file6", extension: "ppt", size: 600_000, date: new Date('2023-05-03') },
+    { bagId: "8A3BFBAC38A688322EC507C06F90D5FA74DEECEEEEB2E40666D25DA89291F4945", name: "my-book", extension: "epub", size: 600_000, date: new Date('2023-05-02') },
+    { bagId: "8A3BFBAC38A688322EC507C06F90D5FA74DEECEEEEB2E40666D25DA89291F4936", name: "file7", extension: "zip", size: 700_000_000, date: new Date('2023-04-03') },
+    { bagId: "8A3BFBAC38A688322EC507C06F90D5FA74DEECEEEEB2E40666D25DA89291F4937", name: "file8", extension: "docx", size: 800_000_000, date: new Date('2023-03-03') },
+    { bagId: "8A3BFBAC38A688322EC507C06F90D5FA74DEECEEEEB2E40666D25DA89291F4938", name: "file9", extension: "mp4", size: 900_000_000, date: new Date('2023-02-03') },
   ];
 }
