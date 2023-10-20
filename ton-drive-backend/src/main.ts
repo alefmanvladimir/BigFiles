92551dd2f38fdab88856224a7cc69c55d2422430
Nikolay Dorofeev <dorich2000@gmail.com>
2023-10-20 18:22:01 +0400

    Allow access from different origins

--- ton-drive-backend/src/main.ts
+++ ton-drive-backend/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: { origin: '*' } });
   await app.listen(3000);
 }
 bootstrap();
