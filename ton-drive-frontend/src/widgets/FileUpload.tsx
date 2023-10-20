a5bb09781cfee7447d6029a2a413a0f528426502
Nikolay Dorofeev <dorich2000@gmail.com>
2023-10-20 18:18:59 +0400

    Temporal upload file form

---
+++ ton-drive-frontend/src/widgets/FileUpload.tsx
@@ -0,0 +1,32 @@
+import { type FormEvent } from "react"
+
+export interface FileUploadProps {
+  className?: string;
+}
+
+export default function FileUpload({ className = '' }: FileUploadProps) {
+
+  async function handleFileUpload(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const file = formData.get('file')
+    console.log(file)
+
+    const response = await fetch('http://127.0.0.1:3000/upload', {
+      method: 'POST',
+      body: formData
+    });
+    console.log(response)
+  }
+
+  return (
+    <>
+      <form onSubmit={handleFileUpload} encType='multipart/form-data' className={`form-control gap-y-2 ${className}`}>
+        <input type='file' name='file' className="file-input file-input-bordered file-input-accent w-full max-w-xs" />
+        <button type='submit' className="btn btn-accent">
+          Upload
+        </button>
+      </form>
+    </>
+  )
+}
