3962402c53baba23119bf3fa01482a83611c1a50
mitagmio <mitag.mio@gmail.com>
2023-10-27 17:29:46 +0000

    chore: for test

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
  #!/bin/bash
 cd ton-drive-backend
 docker-compose down && docker builder prune -a -f && docker-compose up -d --build
-sleep 1
 cd .. && cd ton-drive-frontend
 docker-compose down && docker builder prune -a -f && docker-compose up -d --build
