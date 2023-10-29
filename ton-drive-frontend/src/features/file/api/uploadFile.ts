bf083506c510851a1d5929c0c5dd88c578b6f85d
Nikolay Dorofeev <dorich2000@gmail.com>
2023-10-29 19:21:10 +0400

    Show upload indicator

---
+++ ton-drive-frontend/src/features/file/api/uploadFile.ts
@@ -0,0 +1,53 @@
+import { Sender, TonClient } from "ton";
+import {Address, Cell} from "ton-core";
+import tonDrive, {storageProviderAddress} from "../../../services/FilesService";
+
+export interface FileUploadOptions {
+  file: File;
+  tonClient: {
+    client: TonClient | undefined;
+  };
+  sender: Sender;
+  wallet: string;
+}
+
+export async function uploadFile({ file, tonClient, sender, wallet }: FileUploadOptions) {
+  const host = 'https://api.bigfiles.cloud'
+
+  const formData = new FormData()
+  formData.append('file', file)
+
+  if (!tonClient || !sender || !wallet) {
+      console.log("Wallet not connected")
+      return;
+  }
+  const tonDriveService = tonDrive(tonClient.client!!, sender)
+  const response = await fetch(`${host}/upload`, {
+      method: 'POST',
+      body: formData
+  })
+
+  const {bagId} = await response.json() as {bagId: string}
+  const contractParams = new URLSearchParams()
+  contractParams.append('bagId', bagId)
+  contractParams.append('providerAddress', storageProviderAddress.toRawString())
+  // TODO: use POST method body instead of query params
+  const contractResponse = await fetch(new URL('/contracts?' + contractParams.toString(), host).toString(), {
+    method: 'POST'
+  })
+  const contractFile = await contractResponse.arrayBuffer()
+
+  const base64 = btoa(new Uint8Array(contractFile)
+      .reduce((data, byte) => data + String.fromCharCode(byte), ''))
+
+  const msgBody = Cell.fromBase64(base64)
+  const slice = msgBody.beginParse()
+  const opQueryId = slice.loadUint(32 + 64)
+  const torrentHash = slice.loadRef().hash().toString('hex')
+  console.log(`HEX: ${torrentHash}`, BigInt(`0x${torrentHash}`))
+  const userCollection = tonDriveService.userCollection(Address.parse(wallet))
+  return userCollection.createContract(msgBody, {
+    name: file.name,
+    size: file.size
+  })
+}
--- ton-drive-frontend/src/widgets/FileUploadCard.tsx
+++ ton-drive-frontend/src/widgets/FileUploadCard.tsx
@@ -1,15 +1,16 @@
-import {type FormEvent, useEffect} from "react"
-import {Address, Cell} from "ton-core";
+import { useState } from "react";
+import {type FormEvent} from "react"
 import {useTonClient} from "../shared/hooks/useTonClient";
 import {useTonConnect} from "../shared/hooks/useTonConnect";
-import tonDrive, {storageProviderAddress} from "../services/FilesService";
 import {useTonAddress} from "@tonconnect/ui-react";
+import { uploadFile } from "../features/file/api/uploadFile";
 
 export interface FileUploadProps {
     className?: string;
 }
 
 export default function FileUpload({className = ''}: FileUploadProps) {
+  const [isFetching, setIsFetching] = useState(false)
   const tonClient = useTonClient()
   const sender = useTonConnect().sender
   const wallet = useTonAddress()
@@ -20,7 +21,7 @@ export default function FileUpload({className = ''}: FileUploadProps) {
     const file = formData.get('file')
 
     if (file == null) {
-        console.log("File not chosen")
+        console.error("File not chosen")
         return;
     }
 
@@ -29,52 +30,32 @@ export default function FileUpload({className = ''}: FileUploadProps) {
       return
     }
 
-    const host = 'https://api.bigfiles.cloud'
-    if (!tonClient || !sender || !wallet || wallet === '') {
-        console.log("Wallet not connected")
-        return;
+    if (file.size === 0) {
+      console.error('File is empty')
+      return
     }
-    const tonDriveService = tonDrive(tonClient.client!!, sender)
-    const response = await fetch(`${host}/upload`, {
-        method: 'POST',
-        body: formData
-    })
 
-    const {bagId} = await response.json() as {bagId: string}
-    const contractParams = new URLSearchParams()
-    contractParams.append('bagId', bagId)
-    contractParams.append('providerAddress', storageProviderAddress.toRawString())
-    // TODO: use POST method body instead of query params
-    const contractResponse = await fetch(new URL('/contracts?' + contractParams.toString(), host).toString(), {
-      method: 'POST'
-    })
-    const contractFile = await contractResponse.arrayBuffer()
-
-    const base64 = btoa(new Uint8Array(contractFile)
-        .reduce((data, byte) => data + String.fromCharCode(byte), ''))
-
-    const msgBody = Cell.fromBase64(base64)
-    const slice = msgBody.beginParse()
-    const opQueryId = slice.loadUint(32 + 64)
-    const torrentHash = slice.loadRef().hash().toString('hex')
-    console.log(`HEX: ${torrentHash}`, BigInt(`0x${torrentHash}`))
-    const userCollection = tonDriveService.userCollection(Address.parse(wallet))
-    return userCollection.createContract(msgBody, {
-      name: file.name,
-      size: file.size
-    })
-
+    setIsFetching(true)
+    await uploadFile({ tonClient, sender, wallet, file })
+    setIsFetching(false)
   }
 
   return (
     <div className={`card bg-base-200 ${className}`}>
-      <form onSubmit={handleFileUpload} encType='multipart/form-data' className="card-body gap-y-2">
+      <form
+        onSubmit={handleFileUpload}
+        encType='multipart/form-data'
+        className="card-body gap-y-2"
+        aria-disabled={isFetching}>
         <h2 className="card-title">Upload File</h2>
-        <input type='file' name='file'
+        <input type='file' name='file' disabled={isFetching}
           className="file-input file-input-bordered file-input-accent w-full max-w-xs"/>
         <div className="card-actions justify-end">
-          <button type='submit' className="btn btn-accent btn-sm">
-            Upload
+          <button type='submit' disabled={isFetching}
+            className={`btn btn-accent btn-sm ${isFetching ? 'btn-disabled' : ''}`}>
+            {
+              isFetching ? 'Uploading...' : 'Upload'
+            }
           </button>
         </div>
       </form>
