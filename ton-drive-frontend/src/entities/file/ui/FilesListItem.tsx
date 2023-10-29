e974f5bdf7f4e7fdb12f66ba8e5218d870b1c4f8
Nikolay Dorofeev <dorich2000@gmail.com>
2023-10-29 21:14:28 +0400

    Beautify filelist

--- ton-drive-frontend/src/entities/file/ui/FilesListItem.tsx
+++ ton-drive-frontend/src/entities/file/ui/FilesListItem.tsx
@@ -1,40 +1,50 @@
-import type {TonStorageFile} from "../model/TonStorageFile"
-import FileSize from "./FileSize"
-import FileDate from "./FileDate"
-import FileTypeIcon from "./FileTypeIcon"
+import type { TonStorageFile } from "../model/TonStorageFile";
+import FileSize from "./FileSize";
+import FileDate from "./FileDate";
+import FileTypeIcon from "./FileTypeIcon";
+import ContractLink from "../../../shared/ui/ContractLink";
 
 export interface FilesListItemProps {
-    file: TonStorageFile
-    className?: string
-    actions?: JSX.Element
+  file: TonStorageFile;
+  className?: string;
+  actions?: JSX.Element;
 }
 
-export default function FilesListItem({file, className, actions}: FilesListItemProps) {
-    return (
-        <li className={`card card-compact flex-row items-center gap-2 bg-base-300 p-3 ${className ?? ''}`}>
+export default function FilesListItem({ file, className, actions }: FilesListItemProps) {
+  return (
+    <li className={`card card-compact bg-base-300 p-3 ${className ?? ""}`}>
+      <div className="font-bold text-md">
+        {file.name}
+        <div className="badge badge-neutral ml-1">{file.extension}</div>
+      </div>
+      <div className="flex flex-col sm:flex-row gap-2 ">
+        <div className="flex flex-col sm:flex-row sm:items-center gap-2 text-ellipsis overflow-hidden">
+          <div className="flex items-center gap-2 sm:w-1/4">
             <div>
-                <FileTypeIcon extension={file.extension}/>
+              <FileTypeIcon extension={file.extension} />
             </div>
-            <div className="text-ellipsis overflow-hidden">
-                <div className="card-title">
-                    {file.name}
-                    <div className="badge badge-neutral">{file.extension}</div>
-                </div>
-                <div className="text-accent-content text-sm"><FileSize size={file.size}/> | <FileDate date={file.date}/>
-                </div>
-                <span className="text-accent-content text-sm">{file.bagId}</span>
-                <div>
-                    <span className="text-accent-content text-sm">
-                        Storage Contract: {
-                        file.storageContractInfo.address ? file.storageContractInfo.address?.toString() : <span className={'text-error-content text-sm'}>Not ready</span>
-                    }
-                    </span>
-                </div>
+            <div className="sm:flex sm:flex-col">
+              <div className="text-accent-content text-xs">
+                <FileSize size={file.size} /> | <FileDate date={file.date} />
+              </div>
             </div>
-            {/* Actions */}
-            <div className="ml-auto">
-                {actions}
-            </div>
-        </li>
-    )
+          </div>
+          <table className="table table-xs">
+            <tbody>
+              <tr>
+                <th>Bag ID</th>
+                <td>{file.bagId}</td>
+              </tr>
+              <tr>
+                <th>Contract</th>
+                <td>{file.storageContractInfo.address ? <ContractLink address={file.storageContractInfo.address?.toString()} /> : <span className={"text-error-content text-sm"}>Not ready</span>}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        {/* Actions */}
+        <div className="ml-auto">{actions}</div>
+      </div>
+    </li>
+  );
 }
--- ton-drive-frontend/src/features/file/ui/FilesListWithActions.tsx
+++ ton-drive-frontend/src/features/file/ui/FilesListWithActions.tsx
@@ -9,17 +9,19 @@ export interface FilesListWithActionsProps {
 
 const createActions = (close: (hexBagId: string) => Promise<void>) => (file: TonStorageFile) => {
     return (
-        <>
-            <button className={"btn btn-outline btn-sm btn-primary"} onClick={() => close(file.bagId)}>Close</button>
-            <button className="btn btn-outline btn-accent">
+        <div className="join sm:join-vertical">
+            <button className="join-item btn btn-outline btn-sm btn-primary" onClick={() => close(file.bagId)}>Close</button>
+            <a
+              download href={createDownloadLink(file)} target="_blank"
+              className="join-item btn btn-outline btn-sm btn-accent">
                 {/* Download icon */}
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
                      stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round"
                           d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"/>
                 </svg>
-            </button>
-        </>
+            </a>
+        </div>
     )
 }
 
@@ -34,4 +36,9 @@ export default function FilesListWithActions(
             <FilesList className={className} files={files} createActions={createActions(closeItem)}/>
         </>
     )
+}
+
+function createDownloadLink(file: TonStorageFile) {
+  return 'https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg'
+  return `https://storage.ton.run/gateway/${file.bagId}/${file.name}.${file.extension}`
 }
